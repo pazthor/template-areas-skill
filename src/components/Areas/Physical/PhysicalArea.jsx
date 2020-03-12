3a7943a4d1da51129132978cd1e0a3df75962d1c
Julio <pastor@indava.com>
2020-03-11 18:01:15 -0600

    add props to physical Area

--- src/components/Areas/Physical/PhysicalArea.jsx
+++ src/components/Areas/Physical/PhysicalArea.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import AreaHeader from '../../Base/AreaHeader'
-import AreaBody from '../../Base/AreaBody'
+import React from "react";
+import AreaHeader from "../../Base/AreaHeader";
+import AreaBody from "../../Base/AreaBody";
+import WrapperArea from "../../Base/WrapperArea";
+import WrapperSection from "../../Base/WrapperSection";
+import redirecTo from "../../../utils/redirect";
+const PhysicalArea = params => {
+  const physicalAreaColor = "#1FADDF";
 
-
-const PhysicalArea = (params) => {
-    const physicalAreaColor= '#1FADDF'
-    return (
-        <>
-        <AreaHeader color={physicalAreaColor}></AreaHeader>
-        <AreaBody></AreaBody>        
-        </>
-    )
-}
+  return (
+    <>
+      <WrapperArea>
+        <WrapperSection>
+          <AreaHeader
+            color={physicalAreaColor}
+            isActivePhysical={true}
+            isActiveSocial={false}
+            onClickSocial={() => {
+              redirecTo("social-emocional");
+            }}
+            onClickPhysical={() => {
+              redirecTo("/physical");
+            }}
+            titleSkillText={"aqi va el api medianrte icontext"}
+            bodySkillText={"boddy el text"}
+          ></AreaHeader>
+          <AreaBody></AreaBody>
+        </WrapperSection>
+      </WrapperArea>{" "}
+    </>
+  );
+};
 
 export default PhysicalArea;
