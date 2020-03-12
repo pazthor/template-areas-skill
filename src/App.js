6be7f8195f31e243174323fb7e2c9e1c95a40364
Julio <pastor@indava.com>
2020-03-12 11:24:21 -0600

    Add store to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 import Routes from "./Routes";
 
 function App() {
   return (
     <>
+    <Provider store={store}>
       <BrowserRouter basename="/area">
         <Routes />
       </BrowserRouter>
+      </Provider>,
     </>
   );
 }
